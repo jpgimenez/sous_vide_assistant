d179b236f4ddb6ccc8eb7859920aa440567bca2c
Juan Pablo Giménez <jpg@rosario.com>
2025-06-14 18:32:02 -0300

    some fixes

--- custom_components/sous_vide_assistant/__init__.py
+++ custom_components/sous_vide_assistant/__init__.py
@@ -3,6 +3,6 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
